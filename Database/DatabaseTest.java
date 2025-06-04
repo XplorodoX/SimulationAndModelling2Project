3e82ff8b2b47bbe72b80eed64de1e72fa1d31041
Merluee <merlaufl@gmail.com>
2025-06-04 14:01:33 +0200

    Add CSV import test

--- Database/DatabaseTest.java
+++ Database/DatabaseTest.java
@@ -1,12 +1,12 @@
+import java.io.File;
+
 public class DatabaseTest {
     public static void main(String[] args) throws Exception {
         Databank db = new Databank();
         db.setElectricityPrice(42.0);
 
-        String url = args.length > 0 ? args[0] : null;
-        try (var conn = (url != null)
-                ? AnyLogicDBUtil.openConnection(url)
-                : AnyLogicDBUtil.openHSQLDBConnection()) {
+        String url = args.length > 0 ? args[0] : "jdbc:hsqldb:mem:testdb";
+        try (var conn = AnyLogicDBUtil.openConnection(url)) {
             AnyLogicDBUtil.createSchema(conn);
             AnyLogicDBUtil.insertDatabank(conn, "test", db);
 
@@ -15,6 +15,18 @@ public class DatabaseTest {
                 System.out.println("Test passed: price=" + price);
             } else {
                 System.err.println("Test failed: expected " + db.getElectricityPrice() + " but got " + price);
+            }
+
+            // Import sample CSV file and verify row count
+            File csv = new File("sample_data.csv");
+            AnyLogicDBUtil.importTableFromFile(conn, "sample_table", csv);
+            try (var st = conn.createStatement();
+                 var rs = st.executeQuery("SELECT COUNT(*) FROM sample_table")) {
+                if (rs.next() && rs.getInt(1) == 2) {
+                    System.out.println("CSV import test passed");
+                } else {
+                    System.err.println("CSV import test failed");
+                }
             }
         }
     }
